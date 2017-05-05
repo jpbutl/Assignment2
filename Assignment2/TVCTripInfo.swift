7a1f16f2a9d15bde76d18364030be22d52427f45
JULIAN BUTLER <jpbutler@10-148-140-65.dhcp-m.deakin.edu.au>
2017-05-05 18:12:17 +1000

    Second Commit

---
+++ Assignment2/TVCTripInfo.swift
@@ -0,0 +1,15 @@
+//
+//  TVCTripInfo.swift
+//  Assignment2
+//
+//  Created by JULIAN BUTLER on 5/05/2017.
+//  Copyright © 2017 JULIAN BUTLER. All rights reserved.
+//
+// TVC = TableViewController
+
+import Foundation
+import UIKit
+
+class TblViewControllerTripInfo : UITableViewController
+{
+}
---
+++ Assignment2/TVCTrips.swift
@@ -0,0 +1,14 @@
+//
+//  TVCTrips.swift
+//  Assignment2
+//
+//  Created by JULIAN BUTLER on 5/05/2017.
+//  Copyright © 2017 JULIAN BUTLER. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TblViewControllerTrips: UITableViewController {
+    
+}
